729de03061f6af7621525be6e7c807a8d724cc78
overpathz <overpathz@gmail.com>
2022-05-02 00:00:34 +0300

    refactor SenderApp -- move the code related to command sending into separate class

--- src/main/java/client/SenderApp.java
+++ src/main/java/client/SenderApp.java
@@ -1,67 +1,32 @@
 package client;
 
-import configuration.Configuration;
+import client.controller.Controller;
+import client.sender.CommandSender;
+import client.configuration.Configuration;
 import javafx.application.Application;
-import javafx.event.EventType;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+public class SenderApp extends Application {
 
-import java.io.OutputStream;
-import java.net.Socket;
-import java.util.logging.Logger;
-
-public class FxControl extends Application {
-
-    private static final Logger LOGGER = Logger.getLogger(FxControl.class.getSimpleName());
-    private 
+    private CommandSender commandSender;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(FxControl.class.getResource("../Main.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(SenderApp.class.getResource("../Main.fxml"));
         fxmlLoader.load();
         Scene scene = new Scene(fxmlLoader.getRoot());
+        Controller controller = fxmlLoader.getController();
         primaryStage.setScene(scene);
         primaryStage.setWidth(Configuration.STAGE_WIDTH);
         primaryStage.setHeight(Configuration.STAGE_HEIGHT);
-        applySocketAction(scene, fxmlLoader.getController());
+        commandSender = new CommandSender(controller);
+        applySocketAction(scene);
         primaryStage.show();
     }
 
-    private void applySocketAction(Scene scene, Controller controller) {
-        scene.setOnMouseMoved(event -> );
-        scene.setOnMouseClicked(event -> extracted(event, "/tap/", controller));
-    }
-
-    private void extracted(MouseEvent event, String command, Controller controller) {
-        double xCoefficient = Configuration.REMOTE_WIDTH / Configuration.STAGE_WIDTH;
-        double yCoefficient = Configuration.REMOTE_HEIGHT / Configuration.STAGE_HEIGHT;
-
-        EventType<? extends MouseEvent> eventType = event.getEventType();
-        System.out.println(eventType);
-
-        double layoutX = event.getX();
-        double layoutY = event.getY();
-
-        int x = (int) (layoutX * xCoefficient);
-        int y = (int) (layoutY * yCoefficient);
-
-        String msgToSend = command + x + "/" + y;
-        LOGGER.info(msgToSend);
-        controller.getCoordinates().setText("X: " + layoutX + " | Y: " + layoutY);
-        sendData(msgToSend);
-    }
-
-    private void sendData(String data)  {
-        try {
-            try(Socket socket = new Socket(Configuration.IP, Configuration.PORT);
-                OutputStream outputStream = socket.getOutputStream()) {
-                outputStream.write(data.getBytes());
-                outputStream.flush();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private void applySocketAction(Scene scene) {
+        scene.setOnMouseMoved(commandSender::sendCommand);
+        scene.setOnMouseClicked(commandSender::sendCommand);
     }
 }
